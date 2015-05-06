e99c843746d34ac10a305bf0f0a01230fb7de10c
Willem van den Ende <willem@qwan.eu>
2015-05-06 21:52:52 +0100

    Rename docker build dir to haskell-desktop

--- buildall.sh
+++ buildall.sh
@@ -2,6 +2,6 @@
 set -x
 set -o
 
-docker build -t capitalmatch/hstoolbuilder ./toolbuilder
+docker build -t capitalmatch/hstoolbuilder ./hstoolbuilder
 docker run --rm toolbuilder tar -cvz -C .cabal-sandbox/bin . > big-monolith/haskell-tools.tgz
-docker build -t capitalmatch/haskell-desktop ./big-monolith
+docker build -t capitalmatch/haskell-desktop ./haskell-desktop
