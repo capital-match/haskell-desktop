52365b7822af1e53c42dc4517d97ed06269a8c42
Greg Weber <greg@gregweber.info>
2015-06-03 12:28:32 -0400

    fix buildall.sh script

--- buildall.sh
+++ buildall.sh
@@ -2,6 +2,7 @@
 set -x
 set -o
 
+desktop="haskell-desktop"
 docker build -t capitalmatch/hstoolbuilder ./hs-toolbuilder
-docker run --rm toolbuilder tar -cvz -C .cabal-sandbox/bin . > big-monolith/haskell-tools.tgz
-docker build -t capitalmatch/haskell-desktop ./haskell-desktop
+docker run --rm capitalmatch/hstoolbuilder tar -cvz -C .cabal-sandbox/bin . > $desktop/haskell-tools.tgz
+docker build -t capitalmatch/$desktop ./$desktop
