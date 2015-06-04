3c7a2edf21bb12d0f4f8fee913b4eda21089e7ba
Willem van den Ende <willem@qwan.eu>
2015-06-04 09:33:17 +0100

    Add explicit tag on ghc version

--- buildall.sh
+++ buildall.sh
@@ -3,6 +3,7 @@ set -x
 set -o
 
 desktop="haskell-desktop"
-docker build -t capitalmatch/hstoolbuilder ./hs-toolbuilder
+tag="7.10.2"
+docker build -t capitalmatch/hstoolbuilder:$tag ./hs-toolbuilder
 docker run --rm capitalmatch/hstoolbuilder tar -cvz -C .cabal-sandbox/bin . > $desktop/haskell-tools.tgz
-docker build -t capitalmatch/$desktop ./$desktop
+docker build -t capitalmatch/$desktop:$tag ./$desktop
